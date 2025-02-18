fc86c79bbff34c6daa900dd8f7278e560d7f8ff4
Soham Sheth <sohamashishsheth@gmail.com>
2025-02-18 01:53:57 -0600

    Episode 10

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -1,10 +1,7 @@
-import { useContext } from "react";
 import { CDN_URL } from "../utils/constants";
-import UserContext from "../utils/UserContext";
 
 const RestaurantCard = (props) => {
   const { resData } = props;
-  const { loggedInUser } = useContext(UserContext);
 
   const {
     cloudinaryImageId,
@@ -13,7 +10,7 @@ const RestaurantCard = (props) => {
     cuisines,
     costForTwo,
     deliveryTime,
-  } = resData;
+  } = resData?.data;
 
   return (
     <div className="m-4 p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200">
@@ -27,26 +24,8 @@ const RestaurantCard = (props) => {
       <h4>{avgRating} stars</h4>
       <h4>₹{costForTwo / 100} FOR TWO</h4>
       <h4>{deliveryTime} minutes</h4>
-      <h4>User : {loggedInUser} </h4>
     </div>
   );
-};
-
-// Higher Order Component
-
-// input - RestaurantCard =>> RestaurantCardPromoted
-
-export const withPromtedLabel = (RestaurantCard) => {
-  return (props) => {
-    return (
-      <div>
-        <label className="absolute bg-black text-white m-2 p-2 rounded-lg">
-          Promoted
-        </label>
-        <RestaurantCard {...props} />
-      </div>
-    );
-  };
 };
 
 export default RestaurantCard;
